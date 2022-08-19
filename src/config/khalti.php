e23ab1dc37435f9553459fe3116ccdf4447b60f8
735l4 <pratibimbakhadka@gmail.com>
2022-08-20 02:19:03 +0545

    chore: khalti config init

---
+++ src/config/khalti.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'debug' => true, // set false to run on live khalti url
+    'auto_redirect' => true, // set false if you don't want khalti to auto redirect
+    'website_url' => 'https://example.com', // your website url
+    'live_public_key' => '', // public key from khalti
+    'live_secret_key' => '' // secret key from khalti
+];
