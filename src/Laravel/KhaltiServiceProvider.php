186c36ff82dd5ff191956aa1c17767a5d3b26a23
735l4 <pratibimbakhadka@gmail.com>
2022-08-20 02:19:19 +0545

    feat: khalti service provider with publishable config

---
+++ src/Laravel/KhaltiServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Khalti\Laravel;
+
+use Illuminate\Support\ServiceProvider;
+use Khalti\Khalti;
+
+final class KhaltiServiceProvider extends ServiceProvider {
+
+    public function boot() {
+        $this->publishes([
+            __DIR__.'/../config/khalti.php' => config_path('khalti.php')
+        ], 'khalti-config');
+    }
+
+    public function register() {
+
+        $this->mergeConfigFrom(__DIR__ . '/../config/khalti.php', 'khalti');
+
+        $this->app->singleton(Khalti::class, function () {
+            return new Khalti();
+        });
+
+    }
+}
