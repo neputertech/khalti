ea719624d79e26cfb2863bb30ee772951305dcc9
Rajesh Chaudhary <rajeshisnepali@gmail.com>
2022-11-16 21:09:59 +0545

    refactor: serve secret keys from .env

--- src/config/khalti.php
+++ src/config/khalti.php
@@ -4,6 +4,6 @@ return [
     'debug' => true, // set false to run on live khalti url
     'auto_redirect' => true, // set false if you don't want khalti to auto redirect
     'website_url' => 'https://example.com', // your website url
-    'live_public_key' => '', // public key from khalti
-    'live_secret_key' => '' // secret key from khalti
+    'live_public_key' => env('KHALTI_PUBLIC_KEY'), // public key from khalti
+    'live_secret_key' => env('KHALTI_SECRET_KEY') // secret key from khalti
 ];
