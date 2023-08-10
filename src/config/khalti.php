e02868502506e997e588cd46c4f8295cf6a8341f
pratibimba-neputer <pratibimbakhadka@neputer.com>
2023-08-10 17:52:01 +0545

    ref: remove auto redirect

--- src/config/khalti.php
+++ src/config/khalti.php
@@ -2,7 +2,6 @@
 
 return [
     'debug' => true, // set false to run on live khalti url
-    'auto_redirect' => true, // set false if you don't want khalti to auto redirect
     'website_url' => 'https://example.com', // your website url
     'public_key' => env('KHALTI_PUBLIC_KEY', ''), // public key from khalti
     'secret_key' => env('KHALTI_SECRET_KEY', ''), // secret key from khalti
